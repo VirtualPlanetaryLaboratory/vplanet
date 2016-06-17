682dfe1a2c6d6f65c3cf3a1ec5c08ef68c06b07c
Rodrigo Luger <rodluger@gmail.com>
2016-06-17 14:08:28 -0700

    debug

--- vplot/setup.py
+++ vplot/setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import division, print_function, absolute_import, unicode_literals
+from __future__ import division, print_function, absolute_import
 from setuptools import setup
 from setuptools.command.install import install
 import warnings

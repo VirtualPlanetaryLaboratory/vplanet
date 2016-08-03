5a04dcee00e2db86bbfb1804ef61968bdaff1412
Rory Barnes <rory@kali.astro.washington.edu>
2016-08-02 17:18:54 -0700

    Added eqtide to testing directory.

---
+++ testing/eqtide/test.py
@@ -0,0 +1,6 @@
+params = ['final.star.RotPer', 
+          'final.d.RotPer', 
+          'final.d.Obliquity',
+          'final.d.Eccentricity',
+          'final.d.SemiMajorAxis']
+tolerance = 1.e-5

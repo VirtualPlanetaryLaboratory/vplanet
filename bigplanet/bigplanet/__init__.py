6ee9bc8f1699a21b59ecb1acc9e2dd8a316f379e
David Fleming <dflemin3@uw.edu>
2017-02-22 11:23:12 -0800

    parallel bigplanet verified to work; example ipynb added

--- bigplanet/bigplanet/__init__.py
+++ bigplanet/bigplanet/__init__.py
@@ -9,7 +9,7 @@ bigplanet package init file.
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-__version__ = "0.19"
+__version__ = "0.2"
 __author__ = "David Fleming (dflemin3@uw.edu)"
 __copyright__ = "Copyright 2017 David P. Fleming"
 

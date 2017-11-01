fb8d891c4391cdddbb26da9754485efff0699d8f
Rory Barnes <rory@D-108-179-185-92.dhcp4.washington.edu>
2017-11-01 11:53:11 -0700

    Re-added binary_eqtide_stellar/.

---
+++ testing/binary_eqtide_stellar/test.py
@@ -0,0 +1,6 @@
+params = ['final.cbp.FreeEcc', 
+          'final.cbp.Eccentricity', 
+          'final.cbp.SemiMajorAxis',
+          'final.secondary.Eccentricity',
+          'final.secondary.SemiMajorAxis']
+tolerance = 1.e-5

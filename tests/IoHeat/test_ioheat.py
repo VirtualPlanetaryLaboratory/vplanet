5d5aff7247c6a3eacaae8c97b0cdbc454fe5a05a
Rory Barnes <rory@Rorys-MBP-2.domain>
2018-08-08 14:56:01 -0700

    REmoved extraneous line from test_ioheat.py

--- tests/IoHeat/test_ioheat.py
+++ tests/IoHeat/test_ioheat.py
@@ -5,17 +5,16 @@ import os
 cwd = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_stellar():
+def test_ioheat():
     """Test the stellar module."""
     # Run vplanet
     subprocess.run(['vplanet', 'vpl.in', '-q'], cwd=cwd)
 
     # Grab the output
     output = GetOutput(path=cwd)
-(SurfEnFluxEqtide) Surface Energy Flux due to Tides in EQTIDE [kg*m^2/sec^2/(m^2*sec)]: 2.243506
 
     # Check
     assert np.isclose(output.log.initial.io.SurfEnFluxEqtide, 2.243506)
 
 if __name__ == "__main__":
-    test_io_heat()
+    test_ioheat()

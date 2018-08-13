650e4d0b9c3b389771c0592c9d5074bb6e68756b
Rodrigo Luger <rodluger@gmail.com>
2018-08-13 12:07:19 -0700

    initial python interface setup [skip ci]

---
+++ src/vplanet.py
@@ -0,0 +1,44 @@
+import ctypes
+import os
+
+OPTLEN = 24
+OPTDESCR = 128
+OPTLONDESCR = 2048
+MAXFILES = 24
+
+
+class OPTIONS(ctypes.Structure):
+    """Input options struct."""
+
+    _fields_ = [("cName", ctypes.c_char * OPTLEN),
+                ("cDescr", ctypes.c_char * OPTDESCR),
+                ("cLongDescr", ctypes.c_char * OPTLONDESCR),
+                ("cValues", ctypes.c_char * OPTDESCR),
+                ("iType", ctypes.c_int),
+                ("cDefault", ctypes.c_char * OPTDESCR),
+                ("dDefault", ctypes.c_double),
+                ("iModuleBit", ctypes.c_int),
+                ("iMultiFile", ctypes.c_int),
+                ("iMultiIn", ctypes.c_int),
+                ("iLine", ctypes.POINTER(ctypes.c_double)),
+                ("iFile", ctypes.POINTER(ctypes.c_int)),
+                ("cFile", (ctypes.c_char * MAXFILES) * OPTLEN),
+                ("bNeg", ctypes.c_int),
+                ("cNeg", ctypes.c_char * OPTDESCR),
+                ("iFileType", ctypes.c_int),
+                ("dNeg", ctypes.c_double)]
+
+    def __init__(self, **kwargs):
+        pass
+
+
+lib = ctypes.CDLL(os.path.join(os.path.dirname(
+                  os.path.dirname(os.path.abspath(__file__))),
+    'vplanetlib.so'))
+
+
+_HelpOptions = lib.HelpOptions
+_HelpOptions.argtypes = [ctypes.POINTER(OPTIONS), ctypes.c_int]
+
+options = OPTIONS()
+_HelpOptions(options, 0)

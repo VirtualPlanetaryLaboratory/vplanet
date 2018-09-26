5be8f28ab15f8cccbc53f90621cfc4803f337ad5
Rory Barnes <rory@Rorys-MBP-2.domain>
2018-09-26 12:10:28 -0700

    Fixed the tests/IoHeat directory with help from Rodrigo.

--- tests/IoHeat/test_IoHeat.py
+++ tests/IoHeat/test_IoHeat.py
@@ -14,11 +14,11 @@ def test_IoHeat():
     output = GetOutput(path=cwd)
 
     # Run our comparisons
-    #assert np.isclose(output.log.initial.PowerEqtide, 9.380954e+13)
-    assert np.isclose(output.log.initial.SurfEnFluxEqtide, 2.243481)
-    assert np.isclose(output.log.initial.DsemiDtEqtide, -1.671102e-09)
-    assert np.isclose(output.log.initial.DeccDtEqtide, -8.503575e-16)
-    assert np.isclose(output.log.initial.DOblDtEqtide, -3.792877e-13)
+    assert np.isclose(output.log.initial.io.PowerEqtide, 9.380954e+13)
+    assert np.isclose(output.log.initial.io.SurfEnFluxEqtide, 2.243481)
+    assert np.isclose(output.log.initial.io.DsemiDtEqtide, -1.671102e-09)
+    assert np.isclose(output.log.initial.io.DeccDtEqtide, -8.503575e-16)
+    assert np.isclose(output.log.initial.io.DOblDtEqtide, -3.792877e-13)
 
 if __name__ == "__main__":
     test_IoHeat()

b9732ab75ec05a53461987f758700444aae360b1
Rory Barnes <rory@Rorys-MBP-2.domain>
2018-09-26 13:24:20 -0700

    Added tests/BinaryTides.

---
+++ tests/BinaryTides/test_BinaryTides.py
@@ -0,0 +1,25 @@
+from vplot import GetOutput
+import subprocess
+import numpy as np
+import os
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+
+def test_BinaryTides():
+    """Test tidal/stellar evolution of tight binary star systems."""
+    # Run vplanet
+    subprocess.run(['vplanet', 'vpl.in', '-q'], cwd=cwd)
+
+    # Grab the output
+    output = GetOutput(path=cwd)
+
+    # Run our comparisons
+    assert np.isclose(output.log.final.primary.RadGyra, 0.451302)
+    assert np.isclose(output.log.final.primary.RotPer, 3.577631)
+    assert np.isclose(output.log.final.secondary.RotPer, 3.577631)
+    assert np.isclose(output.log.final.secondary.RadGyra, 0.451302)
+    assert np.isclose(output.log.final.secondary.Eccentricity, 0.045546)
+    assert np.isclose(output.log.final.secondary.SemiMajorAxis, 0.057678)
+
+if __name__ == "__main__":
+    test_BinaryTides()

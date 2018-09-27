21d9802d0744e399799adb0cdd846f54b4b334b2
Rory Barnes <rory@Rorys-MBP-2.domain>
2018-09-26 17:09:34 -0700

    Added tests/O2BuildUp.

---
+++ tests/O2BuildUp/test_O2BuildUp.py
@@ -0,0 +1,26 @@
+from vplot import GetOutput
+import subprocess
+import numpy as np
+import os
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+
+def test_O2BuildUp():
+    """Test oxygen build up, HZ limits."""
+    # Run vplanet
+    subprocess.run(['vplanet', 'vpl.in', '-q'], cwd=cwd)
+
+    # Grab the output
+    output = GetOutput(path=cwd)
+
+    # Check
+    assert np.isclose(output.log.final.star.Luminosity, 2.013347e+23)
+    assert np.isclose(output.log.final.star.LXUVStellar, 2.774075e+19)
+    assert np.isclose(output.log.final.star.Radius, 7.770586e+07)
+    assert np.isclose(output.log.final.star.Temperature, 2618.768109)
+    assert np.isclose(output.log.final.e.SurfWaterMass, 3.267768)
+    assert np.isclose(output.log.final.star.HZLimRecVenus, 2.811125e+09)
+    assert np.isclose(output.log.final.star.HZLimEarlyMars, 7.885448e+09)
+
+if __name__ == "__main__":
+    test_O2BuildUp()

f54750520369518f932fd03e56c367a7ad94984a
Rory Barnes <rory@Rorys-MBP-2.domain>
2018-09-28 17:40:17 -0700

    Removing .in files from tests/GalHabit. I think the random stellar encounters are not good for CI testing!

--- tests/BinaryTides/test_BinaryTides.py
+++ tests/BinaryTides/test_BinaryTides.py
@@ -16,7 +16,7 @@ def test_BinaryTides():
     # Run our comparisons
     assert np.isclose(output.log.final.primary.RadGyra, 0.451302)
     assert np.isclose(output.log.final.primary.RotPer, 3.577631, rtol=1e-4)
-    assert np.isclose(output.log.final.secondary.RotPer, 3.577631)
+    assert np.isclose(output.log.final.secondary.RotPer, 3.577631, rtol=1e-4)
     assert np.isclose(output.log.final.secondary.RadGyra, 0.451302)
     assert np.isclose(output.log.final.secondary.Eccentricity, 0.045546)
     assert np.isclose(output.log.final.secondary.SemiMajorAxis, 0.057678)

abd19aa668d454c5f63118c80be9a9282e87ba1d
David Fleming <dflemin3@uw.edu>
2018-10-08 14:31:40 -0700

    improved vplanet paper stellar figure

--- examples/MainSequence/makeplot.py
+++ examples/MainSequence/makeplot.py
@@ -14,7 +14,7 @@ dAge                      1e7
 sStellarModel             baraffe
 dSatXUVFrac               1.e-3
 dSatXUVTime               -0.1
-saOutputOrder Age -Luminosity -Radius Temperature -RotPer LXUVStellar
+saOutputOrder Age -Luminosity -Radius Temperature -RotPer -LXUVStellar RadGyra
 """
 
 system = """#
@@ -33,7 +33,7 @@ bDoForward                1
 bVarDt                    1
 dEta                      0.1
 dStopTime                 5e9
-dOutputTime               1e7
+dOutputTime               5e6
 """
 
 
@@ -63,7 +63,8 @@ def run(masses):
     lum = [output.bodies[n].Luminosity for n in range(len(masses))]
     lxuv = [output.bodies[n].LXUVStellar for n in range(len(masses))]
     prot = [output.bodies[n].RotPer for n in range(len(masses))]
-    return age, radius, lum, lxuv, temp, prot
+    rg = [output.bodies[n].RadGyra for n in range(len(masses))]
+    return age, radius, lum, lxuv, temp, prot, rg
 
 # Check correct number of arguments
 if (len(sys.argv) != 2):
@@ -76,28 +77,42 @@ if (sys.argv[1] != 'pdf' and sys.argv[1] != 'png'):
     exit(1)
 
 # Create the figure
-fig, ax = pl.subplots(2, 2, figsize=(10, 6))
+fig, ax = pl.subplots(nrows=3, ncols=2, figsize=(10, 6))
 fig.subplots_adjust(right=0.825, wspace=0.30)
 
-masses = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-age, radius, lum, lxuv, temp, prot = run(masses)
+masses = np.array([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+age, radius, lum, lxuv, temp, prot, rg = run(masses)
 
 for n, m in enumerate(masses):
+    # Top row: radius, legend
     ax[0, 0].plot(age, radius[n], label="%.1f" % m, color=cmap(0.8 * m))
-    ax[0, 1].plot(age, lum[n], label="%.1f" % m, color=cmap(0.8 * m))
-    ax[1, 0].plot(age, temp[n], label="%.1f" % m, color=cmap(0.8 * m))
-    ax[1, 1].plot(age, prot[n], label="%.1f" % m, color=cmap(0.8 * m))
+    # Dummy data for legend
+    ax[0, 1].plot([101], [100], label="%.1f" % m, color=cmap(0.8 * m))
+
+    # Middle row: rg, Teff
+    ax[1, 0].plot(age, rg[n], label="%.1f" % m, color=cmap(0.8 * m))
+    ax[1, 1].plot(age, temp[n], label="%.1f" % m, color=cmap(0.8 * m))
+
+    # Bottom row: L, Lxuv
+    ax[2, 0].plot(age, lum[n], label="%.1f" % m, color=cmap(0.8 * m))
+    ax[2, 1].plot(age, lxuv[n], label="%.1f" % m, color=cmap(0.8 * m))
 
 for axis in ax.flatten():
     axis.set_xscale('log')
-ax[0, 1].set_yscale('log')
-ax[1, 0].set_xlabel('Time (Gyr)', fontsize=14)
-ax[1, 1].set_xlabel('Time (Gyr)', fontsize=14)
-ax[0, 0].set_ylabel(r'Radius ($\mathrm{R}_\oplus$)', fontsize=14)
-ax[0, 1].set_ylabel(r'Luminosity ($\mathrm{L}_\oplus$)', fontsize=14)
-ax[1, 0].set_ylabel(r'Temperature ($\mathrm{K}$)', fontsize=14)
-ax[1, 1].set_ylabel(r'Rotation Period (days)', fontsize=14)
-leg = ax[1, 1].legend(loc=(1.1, 0.5), title='Mass ($\mathrm{M}_\oplus$)')
+ax[0, 1].set_xlim(0, 1)
+ax[0, 1].set_xlim(0, 1)
+ax[0, 1].set_axis_off()
+ax[2, 0].set_yscale('log')
+ax[2, 1].set_yscale('log')
+ax[2, 0].set_xlabel('Time (yr)', fontsize=14)
+ax[2, 1].set_xlabel('Time (yr)', fontsize=14)
+ax[0, 0].set_ylabel(r'Radius ($\mathrm{R}_\odot$)', fontsize=14)
+ax[1, 0].set_ylabel(r'Radius of Gyration', fontsize=14)
+ax[2, 0].set_ylabel(r'Luminosity ($\mathrm{L}_\odot$)', fontsize=14)
+ax[1, 1].set_ylabel(r'Temperature ($\mathrm{K}$)', fontsize=14)
+ax[2, 1].set_ylabel(r'L$_{\mathrm{XUV}}$ ($\mathrm{L}_\odot$)', fontsize=14)
+leg = ax[0, 1].legend(loc=(-0.11, 0.0), title='Mass ($\mathrm{M}_\odot$)',
+                      ncol=3)
 leg.get_title().set_fontweight('bold')
 
 if (sys.argv[1] == 'pdf'):

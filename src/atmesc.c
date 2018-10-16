7a42a2ab94b9f68dd76ed54a3c335bfc1fb7023f
Rory Barnes <rory@Rorys-MBP-2.domain>
2018-10-15 23:01:40 -0700

    Removed print statement from LogBodyAtmEsc.

--- src/atmesc.c
+++ src/atmesc.c
@@ -2223,8 +2223,6 @@ void LogBodyAtmEsc(BODY *body,CONTROL *control,OUTPUT *output,SYSTEM *system,UPD
 
   for (iOut=OUTSTARTATMESC;iOut<OUTENDATMESC;iOut++) {
     if (output[iOut].iNum > 0) {
-      printf("%d\n",iOut);
-      fflush(stdout);
       WriteLogEntry(body,control,&output[iOut],system,update,fnWrite[iOut],fp,iBody);
     }
   }

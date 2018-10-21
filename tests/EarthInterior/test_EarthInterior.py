ea6eb8e9ef412453a6339bd53d3ed021cea965ba
David Fleming <dflemin3@uw.edu>
2018-10-21 15:00:36 -0700

    updated tests to match slightly improved stellar behavior

--- tests/EarthInterior/test_EarthInterior.py
+++ tests/EarthInterior/test_EarthInterior.py
@@ -18,8 +18,8 @@ def test_EarthInterior():
     # Check
     assert np.isclose(output.log.final.earth.TMan, 2256.001803)
     assert np.isclose(output.log.final.earth.TCore, 5006.543944)
-    assert np.isclose(output.log.final.earth.RIC, 1.232183e+06)
-    assert np.isclose(output.log.final.earth.RadPowerTotal, 2.426567e+13)
+    assert np.isclose(output.log.final.earth.RIC, 1232306.0)
+    assert np.isclose(output.log.final.earth.RadPowerTotal, 24264970000000.0)
     assert np.isclose(output.log.final.earth.MagMom, 0.995060)
 
 

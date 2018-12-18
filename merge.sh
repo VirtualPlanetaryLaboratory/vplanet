f00c7ef8ad39cb2632e148e8abf66102b47d2602
Rodrigo Luger <rodluger@gmail.com>
2018-12-18 10:50:09 -0500

    fix

--- merge.sh
+++ merge.sh
@@ -1,13 +1,13 @@
 BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 if [ $BRANCH == "dev" ]
 then
-    git pull --no-commit --no-ff private master && \
+    git pull --no-commit --no-ff public master && \
     git checkout $BRANCH README.md && \
     git checkout $BRANCH docs/developers.rst && \
     git checkout $BRANCH docs/genexamples.py && \
-    git commit -m "Merging changes from private branch" && \
-    echo "Success! Please push your changes to origin dev." ||
+    git commit -m "Merging changes from public branch" && \
+    echo "Success! Please push your changes to origin dev and issue a pull request for master." ||
     echo "Merge was not performed."
 else
-    echo "Merging from private must be done on the dev branch."
+    echo "Merging from public must be done on the dev branch."
 fi

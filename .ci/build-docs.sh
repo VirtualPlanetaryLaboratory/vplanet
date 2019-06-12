208e72454889c6f529a7f4314a7a5269da4e2988
Rodrigo Luger <rodluger@gmail.com>
2019-06-12 09:49:38 -0400

    still tweaking docs build

--- .ci/build-docs.sh
+++ .ci/build-docs.sh
@@ -23,17 +23,19 @@ rev=$(git rev-parse --short HEAD)
 cd $TRAVIS_BUILD_DIR/docs/.build/html
 git init
 touch .nojekyll
-git add -f .nojekyll > /dev/null 2>&1
-git add -f *.html > /dev/null 2>&1
-git add -f *.js > /dev/null 2>&1
-git add -f _sources > /dev/null 2>&1
-git add -f _static > /dev/null 2>&1
-git add -f _images > /dev/null 2>&1
-git add -f examples > /dev/null 2>&1
-git add -f bugs > /dev/null 2>&1
-git add -f src > /dev/null 2>&1
-git add -f $COVERAGE_HASH > /dev/null 2>&1
-git add -f docs.log > /dev/null 2>&1
+unset -e
+git add -f .nojekyll
+git add -f *.html
+git add -f *.js
+git add -f _sources
+git add -f _static
+git add -f _images
+git add -f examples
+git add -f bugs
+git add -f src
+git add -f $COVERAGE_HASH
+git add -f docs.log
+set -e
 git -c user.name='sphinx' -c user.email='sphinx' commit -m "rebuild gh-pages at ${rev}"
 git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:gh-pages
 

349210b0335a743ec4a4926c1692213b7326574f
Deitrick <deitrick@staff-215-177.eduroam.unibe.ch>
2019-07-24 15:26:46 +0200

    testing a fix in read options

--- src/distrot.h
+++ src/distrot.h
@@ -19,6 +19,8 @@
 #define OPT_PRECRATE              1404
 #define OPT_READORBITDATA         1405
 #define OPT_FILEORBITDATA         1406
+#define OPT_SPECMOMINERTIA        1407
+
 
 /* DISTROT 1400 - 1499 */
 /* System properties 1400-1419, body properties 1420-1499 */

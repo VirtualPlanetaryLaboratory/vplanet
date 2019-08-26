da27a1e77e457d0cf11386e642687756f3948c4b
Rory Barnes <rory@kali.astro.washington.edu>
2019-08-26 13:16:03 -0700

    Updated tests AbioticO2, ApseLock, and AtmEscKepler-36.

--- tests/AbioticO2/test_AbioticO2.py
+++ tests/AbioticO2/test_AbioticO2.py
@@ -8,21 +8,28 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 def test_AbioticO2():
     """Test oxygen build up, HZ limits."""
     # Remove old log file
-    subprocess.run(['rm', 'trappist1.log'], cwd=cwd)
+    subprocess.run(['rm', 'AbioticO2.log'], cwd=cwd)
     # Run vplanet
     subprocess.run(['vplanet', 'vpl.in', '-q'], cwd=cwd)
 
     # Grab the output
     output = GetOutput(path=cwd)
 
-    # Check
-    assert np.isclose(output.log.final.star.Luminosity, 7.362896e+23)
-    assert np.isclose(output.log.final.star.LXUVStellar, 7.362896e+20)
-    assert np.isclose(output.log.final.star.Radius, 1.186507e+08)
-    assert np.isclose(output.log.final.star.Temperature, 2926.558123)
-    assert np.isclose(output.log.final.e.SurfWaterMass, 7.403340, rtol=1e-4)
-    assert np.isclose(output.log.final.star.HZLimRecVenus, 5.349670e+09)
-    assert np.isclose(output.log.final.star.HZLimEarlyMars, 1.482975e+10)
+    # Primary Variables
+    # Star
+    assert np.isclose(output.log.final.star.Luminosity, 7.362970e+23)
+    assert np.isclose(output.log.final.star.LXUVStellar, 7.362970e+20)
+    assert np.isclose(output.log.final.star.Radius, 1.186513e+08)
+    assert np.isclose(output.log.final.star.Temperature, 2926.559809)
+    assert np.isclose(output.log.final.star.RadGyra, 0.466090)
+    # Planet
+    assert np.isclose(output.log.final.e.SurfWaterMass, 7.403320, rtol=1e-4)
+    assert np.isclose(output.log.final.e.OxygenMass, 418.194151)
+
+    # Other checks
+    assert np.isclose(output.log.final.e.FXUV, 3.053313)
+    assert np.isclose(output.log.final.e.AtmXAbsEffH2O, 0.051776)
+
 
 if __name__ == "__main__":
     test_AbioticO2()
--- tests/AtmEscKepler-36/test_AtmEscKepler36.py
+++ tests/AtmEscKepler-36/test_AtmEscKepler36.py
@@ -8,15 +8,15 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 def test_AtmEscKepler36():
     """Test hydrogen envelope loss."""
     # Remove old log file
-    subprocess.run(['rm', 'kepler36.log'], cwd=cwd)
+    subprocess.run(['rm', 'AtmEscKepler-36.log'], cwd=cwd)
     # Run vplanet
     subprocess.run(['vplanet', 'vpl.in', '-q'], cwd=cwd)
 
     # Grab the output
     output = GetOutput(path=cwd)
 
-    # Run our comparisons
-    assert np.isclose(output.log.final.star.LXUVStellar, 3.120390e+21)
+    # Checks
+    # Primary Variables
     assert np.isclose(output.log.final.p10.EnvelopeMass, 3.314569)
     assert np.isclose(output.log.final.p10.Radius, 3.376736e+07)
 

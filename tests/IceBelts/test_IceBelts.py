54c5892f221ea6f7848da577d994512c29db78a8
Rory Barnes <rory@kali.astro.washington.edu>
2019-12-13 15:11:05 -0800

    Finished IceBelts test.

--- tests/IceBelts/test_IceBelts.py
+++ tests/IceBelts/test_IceBelts.py
@@ -18,18 +18,19 @@ def test_IceBelts():
     # Grab the output
     output = GetOutput(path=cwd)
 
-    # Run our comparisons
-    #assert np.isclose(output.log.final.earth.TGlobal, 14.649773)
-    #assert np.isclose(output.log.final.earth.Eccentricity, 0.0167)
-    #assert np.isclose(output.log.final.earth.Obliquity, 0.409103)
-    #assert np.isclose(output.log.final.earth.AlbedoGlobal, 0.344298)
-    #assert np.isclose(output.log.final.earth.FluxOutGlobal, 233.918026)
+    # Check global properties
+    assert np.isclose(output.log.final.earth.TotIceMass, 3.071389e+15)
+    assert np.isclose(output.log.final.earth.TotIceBalance, 9.223785e-08)
+    assert np.isclose(output.log.final.earth.AreaIceCov, 0.054040)
+    assert np.isclose(output.log.final.earth.IceAccum, 0.458812)
+    assert np.isclose(output.log.final.earth.IceAblate, -0.343222)
+    assert np.isclose(output.log.final.earth.TempMaxLand, 337.829716)
+    assert np.isclose(output.log.final.earth.TempMaxWater, 294.261368)
+    assert np.isclose(output.log.final.earth.PeakInsol, 1069.614002)
 
     # Check Grid Output
     for f in subprocess.check_output('echo '+dir+'/*.DailyInsol.*',shell=True).split():
       f1 = re.split('\.',re.split('/',f.decode('ascii'))[-1])  #split apart output file
-
-      print(f1)
 
       if len(f1) == 4:
         timestamp = f1[3]
@@ -37,19 +38,43 @@ def test_IceBelts():
         timestamp = f1[3]+'.'+f1[4]
 
       time0 = np.float(timestamp)
-      print(time0)
 
     sysname = f1[0]
     plname = f1[1]
     insolf = dir+'/'+sysname+'.'+plname+'.DailyInsol.'+timestamp
     tempf = dir+'/'+sysname+'.'+plname+'.SeasonalTemp.'+timestamp
     icef = dir+'/'+sysname+'.'+plname+'.SeasonalIceBalance.'+timestamp
+    planckf = dir+'/'+sysname+'.'+plname+'.PlanckB.'+timestamp
+    divf = dir+'/'+sysname+'.'+plname+'.SeasonalDivF.'+timestamp
+    fmeridf = dir+'/'+sysname+'.'+plname+'.SeasonalFMerid.'+timestamp
+    foutf = dir+'/'+sysname+'.'+plname+'.SeasonalFOut.'+timestamp
 
     insol = np.loadtxt(insolf,unpack=True)
     temp = np.loadtxt(tempf,unpack=True)
     ice = np.loadtxt(icef,unpack=True)
+    planck = np.loadtxt(planckf,unpack=True)
+    div = np.loadtxt(divf,unpack=True)
+    fmerid = np.loadtxt(fmeridf,unpack=True)
+    fout = np.loadtxt(fmeridf,unpack=True)
 
-    print(insol)
+    # Check two different latitudes
+    assert np.isclose(insol[1][0], 1055.352482)
+    assert np.isclose(insol[1][75], 329.47451)
+    assert np.isclose(temp[1][0], 22.591643)
+    assert np.isclose(temp[1][75], 8.998626)
+    assert np.isclose(ice[1][0], -0.0016)
+    assert np.isclose(ice[1][75], -0.000306)
+    assert np.isclose(planck[1][0], 2.09)
+    assert np.isclose(planck[1][75], 2.09)
+    assert np.isclose(div[1][0], 34.757649)
+    assert np.isclose(div[1][75], 13.23595)
+    assert np.isclose(fmerid[1][0], 804939200000000.0)
+    assert np.isclose(fmerid[1][75], 309954000000000.0)
+    assert np.isclose(fout[1][0], 804939200000000.0)
+    assert np.isclose(fout[1][75], 309954000000000.0)
+
+    #print(fmerid[1][0])
+    #print(fmerid[1][75])
 
 if __name__ == "__main__":
     test_IceBelts()

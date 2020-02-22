687990e9f71266259442825fb17a43c6de3b4a70
Rory Barnes <rory@kali.astro.washington.edu>
2020-02-21 16:42:47 -0800

    Added README.
    Added figures.

--- examples/AtmEscEqTide/makeplot.py
+++ examples/AtmEscEqTide/makeplot.py
@@ -24,9 +24,11 @@ mpl.rcParams['font.size'] = 10.0
 
 # Run simulations
 os.chdir('WaterCPL')
+print('Running WaterCPL.')
 subprocess.call(['vplanet', 'vpl.in'])
 
 os.chdir('../WaterCTL')
+print('Running WaterCTL.')
 subprocess.call(['vplanet', 'vpl.in'])
 os.chdir('..')
 
@@ -84,9 +86,9 @@ fig.tight_layout()
 #fig.subplots_adjust(hspace=0.05)
 
 if (sys.argv[1] == 'pdf'):
-    plt.savefig('AtmEscEqTideWater.pdf', bbox_inches="tight", dpi=200)
+    plt.savefig('WaterLossTides.pdf', bbox_inches="tight", dpi=200)
 if (sys.argv[1] == 'png'):
-    plt.savefig('AtmEscEqTideWater.png', bbox_inches="tight", dpi=200)
+    plt.savefig('WaterLossTides.png', bbox_inches="tight", dpi=200)
 
 #exit()
 
@@ -94,25 +96,29 @@ if (sys.argv[1] == 'png'):
 ### Second figure: Envelope loss with CPL ###
 
 # Run simulations
-os.chdir('EnvCPL/auto')
+os.chdir('Lopez12CPL/auto')
+print('Running Lopez12CPL-Auto.')
 subprocess.call(['vplanet', 'vpl.in'])
 
 os.chdir('../Bondi')
+print('Running Lopez12CPL-Bondi.')
 subprocess.call(['vplanet', 'vpl.in'])
 
 os.chdir('../ELim')
+print('Running Lopez12CPL-ELim.')
 subprocess.call(['vplanet', 'vpl.in'])
 
 os.chdir('../RR')
+print('Running Lopez12CPL-RR.')
 subprocess.call(['vplanet', 'vpl.in'])
 
 os.chdir('../..')
 
 # Load data
-cplauto = vpl.GetOutput("./EnvCPL/Auto")
-cplbondi = vpl.GetOutput("./EnvCPL/Bondi")
-cplelim = vpl.GetOutput("./EnvCPL/ELim")
-cplrr = vpl.GetOutput("./EnvCPL/RR")
+cplauto = vpl.GetOutput("./Lopez12CPL/Auto")
+cplbondi = vpl.GetOutput("./Lopez12CPL/Bondi")
+cplelim = vpl.GetOutput("./Lopez12CPL/ELim")
+cplrr = vpl.GetOutput("./Lopez12CPL/RR")
 
 # Plot
 fig, axes = plt.subplots(nrows=3, ncols=2)
@@ -203,107 +209,3 @@ if (sys.argv[1] == 'pdf'):
     plt.savefig('Lopez12CPL.pdf', bbox_inches="tight", dpi=200)
 if (sys.argv[1] == 'png'):
     plt.savefig('Lopez12CPL.png', bbox_inches="tight", dpi=200)
-
-exit()
-
-
-
-### Second figure: Zoom-in on Bondi-limited escape ###
-
-# Plot
-fig, axes = plt.subplots(nrows=2, ncols=3, sharex=True, figsize=(16,9))
-
-time = bondi.auto.Time
-
-## Upper left: Envelope mass fraction ##
-axes[0,0].plot(time, bondi.auto.EnvelopeMass/bondi.auto.Mass, color="C0")
-axes[0,0].plot(time, bondi.bondi.EnvelopeMass/bondi.bondi.Mass, color="C3")
-
-# Initial value
-axes[0,0].axhline(bondi.auto.EnvelopeMass[0]/bondi.auto.Mass[0], color="k",
-                  ls="--")
-
-# Format
-axes[0,0].set_ylim(-0.02, 0.52)
-axes[0,0].set_ylabel("Envelope Mass Fraction")
-
-## Upper middle: Envelope mass time derivative ##
-axes[0,1].plot(time, bondi.auto.DEnvMassDt, color="C0", label="AtmEscAuto")
-axes[0,1].plot(time, bondi.bondi.DEnvMassDt, color="C3", label="Bondi-Limited")
-
-# Format
-axes[0,1].set_ylim(-1.0e15, -1.0e9)
-axes[0,1].set_yscale("symlog", linthreshy=0.1)
-axes[0,1].set_ylabel(r"$\dot{M}_{envelope}$ [kg/s]")
-axes[0,1].legend(loc="upper left", framealpha=0, fontsize=12, ncol=1)
-
-## Upper left: planetary mass ##
-axes[0,2].plot(time, bondi.auto.Mass, color="C0")
-axes[0,2].plot(time, bondi.bondi.Mass, color="C3")
-
-# Initial value
-axes[0,2].axhline(bondi.auto.Mass[0], color="k", ls="--")
-
-# Format
-axes[0,2].set_ylabel(r"Mass [M$_{\oplus}$]")
-
-## Lower left: Planet Radius ##
-axes[1,0].plot(time, bondi.auto.PlanetRadius, color="C0", label="")
-axes[1,0].plot(time, bondi.bondi.PlanetRadius, color="C3", label="")
-
-# Initial value
-axes[1,0].axhline(bondi.auto.PlanetRadius[0], color="k", ls="--", label="")
-
-# Format
-axes[1,0].set_ylim(0, 35)
-axes[1,0].set_ylabel(r"Radius [R$_{\oplus}$]")
-axes[1,0].set_xlabel("Time [yr]")
-
-## Lower middle: Critical FXUV ##
-axes[1,1].plot(time, bondi.auto.RRCriticalFlux, color="C0", label="")
-axes[1,1].plot(time, bondi.bondi.RRCriticalFlux, color="C3", label="")
-axes[1,1].plot(time, bondi.auto.FXUV, color="k", label="Incident\nXUV Flux")
-
-# Format
-axes[1,1].set_xlabel("Time [yr]")
-axes[1,1].set_ylim(1.0e-2, 200)
-axes[1,1].set_yscale("log")
-axes[1,1].set_ylabel(r"Critical XUV Flux [W/m$^2$]")
-axes[1,1].legend(bbox_to_anchor=[0.4, 0.8], framealpha=0, fontsize=13)
-
-## Lower right: Escape regime ##
-axes[1,2].plot(time, bondi.auto.HEscapeRegime, lw=2, color="C0", label="")
-axes[1,2].plot(time, bondi.bondi.HEscapeRegime, lw=2, color="C3", label="")
-
-# Annotate with the regime
-axes[1,2].text(0.7, 0.8, 'Envelope Lost', horizontalalignment='left',
-               verticalalignment='center', transform=axes[1,2].transAxes, fontsize=14)
-axes[1,2].text(0.7, 0.58, 'RR-Limited', horizontalalignment='left',
-               verticalalignment='center', transform=axes[1,2].transAxes, fontsize=14)
-axes[1,2].text(0.03, 0.48, 'Bondi-Limited', horizontalalignment='left',
-               verticalalignment='center', transform=axes[1,2].transAxes, fontsize=14)
-
-# Format
-axes[1,2].set_xlabel("Time [yr]")
-axes[1,2].set_ylabel("Escape Regime")
-axes[1,2].set_yticks([])
-axes[1,2].set_ylim(1, 10)
-
-# Format all axes
-for ax in axes.flatten():
-
-    # Format x axis
-    ax.set_xlim(time[0],time.max())
-    #ax.set_xscale("log")
-
-    # Set rasterization
-    ax.set_rasterization_zorder(0)
-
-fig.tight_layout()
-fig.subplots_adjust(wspace=0.25)
-fig.subplots_adjust(hspace=0.05)
-
-if (sys.argv[1] == 'pdf'):
-    plt.savefig('AtmEscRegimesZoom.pdf', bbox_inches="tight", dpi=200)
-if (sys.argv[1] == 'png'):
-    plt.savefig('AtmEscRegimesZoom.png', bbox_inches="tight", dpi=200)

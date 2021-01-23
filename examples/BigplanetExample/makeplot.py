f75cf158ead0e586d2788cbf600e7e6841b05d31
Rory Barnes <rory@astro.washington.edu>
2021-01-22 18:19:08 -0800

    Changed to plotting inner core radius.
    Added units.

--- examples/BigplanetExample/makeplot.py
+++ examples/BigplanetExample/makeplot.py
@@ -8,25 +8,36 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-HDF5_File = h5.File('ParamterSweep.hdf5', 'r')
+HDF5_File = h5.File('ParameterSweep.hdf5', 'r')
 
-bp.PrintKeys(HDF5_File)
-
-TMan = bp.ExtractColumn(HDF5_File,'earth_TMan_initial')
-Pman = bp.ExtractColumn(HDF5_File,'earth_40KPowerMan_initial')
-
-SurfFLuxTot = bp.ExtractColumn(HDF5_File,'earth_SurfEnFluxTotal_final')
+RIC = bp.ExtractColumn(HDF5_File,'earth_RIC_final')
+RIC_units = bp.ExtractUnits(HDF5_File,'earth_RIC_final')
 
 TMan_uniq = bp.ExtractUniqueValues(HDF5_File,'earth_TMan_initial')
-Pman_uniq = bp.ExtractUniqueValues(HDF5_File,'earth_40KPowerMan_initial')
+TMan_units = bp.ExtractUniqueValues(HDF5_File,'earth_TMan_initial')
 
+TCore_uniq = bp.ExtractUniqueValues(HDF5_File,'earth_TCore_initial')
+TCore_units = bp.ExtractUniqueValues(HDF5_File,'earth_TMan_initial')
 
+RIC_Matrix = bp.CreateMatrix(TMan_uniq,TCore_uniq,RIC)
 
-SurfFLuxTot_Matrix = bp.CreateMatrix(TMan_uniq,Pman_uniq,SurfFLuxTot)
+contours = [1000,1100,1200,1300,1400]
+xlabel = 'Initial Mantle Temperature ('+repr(TMan_units)+')'
+ylabel = 'Initial Core Temperature ('+repr(TCore_units)+')'
+title = 'Final Inner Core Radius ('+repr(RIC_units)+')'
 
-plt.contour(TMan_uniq,Pman_uniq,SurfFLuxTot_Matrix,colors = vpl.colors.pale_blue)
+plt.xlim(2500, 3500)
+plt.ylim(5500, 6500)
+
+plt.xlabel(xlabel,fontsize=20)
+plt.ylabel(ylabel,fontsize=20)
+plt.title(title,fontsize=20)
+
+plt.tick_params(axis='both', labelsize=20)
+
+plt.contour(TMan_uniq,TCore_uniq,RIC_Matrix,colors = vpl.colors.pale_blue,levels=contours)
 
 plt.show()
 plt.close()
 
-plt.savefig('ParameterSweepExample.png')
+plt.savefig('BigPlanetExample.png')

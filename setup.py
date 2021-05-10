5d50b00a89280c1f0d4ae063c54cf318638ca54f
Rodrigo Luger <rodluger@gmail.com>
2021-05-10 13:19:55 -0400

    fix in vplot

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     long_description_content_type="text/markdown",
     license="MIT",
     packages=find_packages(),
-    install_requires=["vplot>=0.3.3"],
+    install_requires=["vplot>=0.3.5"],
     ext_modules=ext_modules,
     cmdclass=cmdclass,
     include_package_data=True,

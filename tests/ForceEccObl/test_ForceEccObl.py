f32dad95c2cfcabc9d34bf8e520f5ea6783645c3
caitlyn-wilhelm <cwilhelm@uw.edu>
2021-05-12 14:29:15 -0700

    fixed some of the tests

--- tests/ForceEccObl/test_ForceEccObl.py
+++ tests/ForceEccObl/test_ForceEccObl.py
@@ -5,14 +5,14 @@ import pytest
 
 @benchmark(
     {
-        "log.final.Earth.Eccentricity": {"value": 0.200000},
-        "log.final.Earth.Obliquity": {"value": 0.855211, "unit": u.rad},
-        "log.final.Earth.AnnInsol": {"value": 310.172861},
-        "log.final.Earth.DivFlux": {"value": -8.595168},
-        "log.final.Earth.EnergyResL": {"value": -0.414500},
-        "log.final.Earth.EnergyResW": {"value": 0.113084},
-        "log.final.Earth.AlbedoLandLat": {"value": 0.508116},
-        "log.final.Earth.AlbedoWaterLat": {"value": 0.309142},
+        "log.final.earth.Eccentricity": {"value": 0.200000},
+        "log.final.earth.Obliquity": {"value": 0.855211, "unit": u.rad},
+        "log.final.earth.AnnInsol": {"value": 310.172861, "unit": u.W / u.m**2},
+        "log.final.earth.DivFlux": {"value": -8.595168, "unit": u.W / u.m**2},
+        "log.final.earth.EnergyResL": {"value": -0.414500},
+        "log.final.earth.EnergyResW": {"value": 0.113084},
+        "log.final.earth.AlbedoLandLat": {"value": 0.508116},
+        "log.final.earth.AlbedoWaterLat": {"value": 0.309142},
 
     }
 )
@@ -29,5 +29,3 @@ class TestForceEccObl(Benchmark):
 #     assert np.isclose(output.log.final.earth.AlbedoLandLat, 0.508116)
 #     assert np.isclose(output.log.final.earth.AlbedoWaterLat, 0.309142)
 #
-# if __name__ == "__main__":
-#     test_ForceEccObl()
--- tests/HabEvapCore/test_HabEvapCore.py
+++ tests/HabEvapCore/test_HabEvapCore.py
@@ -1,47 +1,64 @@
-from vplot import GetOutput
-import subprocess
-import numpy as np
-import os
-cwd = os.path.dirname(os.path.realpath(__file__))
+from benchmark import Benchmark, benchmark
+import astropy.units as u
+import pytest
+
+benchmark(
+    {
+        "log.final.star.LXUVStellar": {"value": 4.988314e+21, "unit": u.kg * u.m**2 / u.sec**3},
+
+        "log.final.ProxCenELim.EnvelopeMass": {"value": 0.000526, "unit" : u.Mearth},
+        "log.final.ProxCenELim.PlanetRadius": {"value": 1.335673, "unit": u.Rearth},
+        "log.final.ProxCenELim.SurfWaterMass": {"value": 1.500000,"unit": u.TO},
+
+        "log.final.ProxCenRRLim.EnvelopeMass": {"value": 0.000300, "unit" : u.Mearth},
+        "log.final.ProxCenRRLim.PlanetRadius": {"value": 1.339401, "unit": u.Rearth},
+        "log.final.ProxCenRRLim.SurfWaterMass": {"value": 1.500000,"unit": u.TO},
+
+        "log.final.ProxCenAuto.EnvelopeMass": {"value": 0.000526, "unit" : u.Mearth},
+        "log.final.ProxCenAuto.PlanetRadius": {"value": 1.335673, "unit": u.Rearth},
+        "log.final.ProxCenAuto.SurfWaterMass": {"value": 1.500000,"unit": u.TO},
+
+        "log.final.LC17ELim.EnvelopeMass": {"value": 0.000000, "unit" : u.Mearth},
+        "log.final.LC17ELim.PlanetRadius": {"value": 1.063513, "unit": u.Rearth},
+        "log.final.LC17ELim.SurfWaterMass": {"value": 1.304141,"unit": u.TO},
+
+        "log.final.LC17RRLim.EnvelopeMass": {"value": 0.000000, "unit" : u.Mearth},
+        "log.final.LC17RRLim.PlanetRadius": {"value": 1.063513, "unit": u.Rearth},
+        "log.final.LC17RRLim.SurfWaterMass": {"value": 1.347668,"unit": u.TO},
+
+        "log.final.LC17Auto.EnvelopeMass": {"value": 0.000000, "unit" : u.Mearth},
+        "log.final.LC17Auto.PlanetRadius": {"value": 1.063513, "unit": u.Rearth},
+        "log.final.LC17Auto.SurfWaterMass": {"value": 1.347368,"unit": u.TO},
+
+    }
+)
+class TestHabEvapCore(Benchmark):
+    pass
 
 
-def test_HabEvapCore():
-    """Test hydrogen envelope loss followed by water loss."""
-    # Remove old log file
-    subprocess.run(['rm', 'proxima.log'], cwd=cwd)
-    # Run vplanet
-    subprocess.run(['vplanet', 'vpl.in', '-q'], cwd=cwd)
-
-    # Grab the output
-    output = GetOutput(path=cwd)
-
-    # Run our comparisons
-    assert np.isclose(output.log.final.star.LXUVStellar, 4.988314e+21)
-
-    assert np.isclose(output.log.final.ProxCenELim.EnvelopeMass, 0.000526)
-    assert np.isclose(output.log.final.ProxCenELim.PlanetRadius, 1.335673)
-    assert np.isclose(output.log.final.ProxCenELim.SurfWaterMass, 1.500000)
-
-    assert np.isclose(output.log.final.ProxCenRRLim.EnvelopeMass, 0.000300)
-    assert np.isclose(output.log.final.ProxCenRRLim.PlanetRadius, 1.339401)
-    assert np.isclose(output.log.final.ProxCenRRLim.SurfWaterMass, 1.500000)
-
-    assert np.isclose(output.log.final.ProxCenAuto.EnvelopeMass, 0.000526)
-    assert np.isclose(output.log.final.ProxCenAuto.PlanetRadius, 1.335673)
-    assert np.isclose(output.log.final.ProxCenAuto.SurfWaterMass, 1.500000)
-
-    assert np.isclose(output.log.final.LC17ELim.EnvelopeMass, 0.000000)
-    assert np.isclose(output.log.final.LC17ELim.PlanetRadius, 1.063513)
-    assert np.isclose(output.log.final.LC17ELim.SurfWaterMass, 1.304141)
-
-    assert np.isclose(output.log.final.LC17RRLim.EnvelopeMass, 0.000000)
-    assert np.isclose(output.log.final.LC17RRLim.PlanetRadius, 1.063513)
-    assert np.isclose(output.log.final.LC17RRLim.SurfWaterMass, 1.347668)
-
-    assert np.isclose(output.log.final.LC17Auto.EnvelopeMass, 0.000000)
-    assert np.isclose(output.log.final.LC17Auto.PlanetRadius, 1.063513)
-    assert np.isclose(output.log.final.LC17Auto.SurfWaterMass, 1.347368)
-
-
-if __name__ == "__main__":
-    test_HabEvapCore()
+    # # Run our comparisons
+    # assert np.isclose(output.log.final.star.LXUVStellar, 4.988314e+21)
+    #
+    # assert np.isclose(output.log.final.ProxCenELim.EnvelopeMass, 0.000526)
+    # assert np.isclose(output.log.final.ProxCenELim.PlanetRadius, 1.335673)
+    # assert np.isclose(output.log.final.ProxCenELim.SurfWaterMass, 1.500000)
+    #
+    # assert np.isclose(output.log.final.ProxCenRRLim.EnvelopeMass, 0.000300)
+    # assert np.isclose(output.log.final.ProxCenRRLim.PlanetRadius, 1.339401)
+    # assert np.isclose(output.log.final.ProxCenRRLim.SurfWaterMass, 1.500000)
+    #
+    # assert np.isclose(output.log.final.ProxCenAuto.EnvelopeMass, 0.000526)
+    # assert np.isclose(output.log.final.ProxCenAuto.PlanetRadius, 1.335673)
+    # assert np.isclose(output.log.final.ProxCenAuto.SurfWaterMass, 1.500000)
+    #
+    # assert np.isclose(output.log.final.LC17ELim.EnvelopeMass, 0.000000)
+    # assert np.isclose(output.log.final.LC17ELim.PlanetRadius, 1.063513)
+    # assert np.isclose(output.log.final.LC17ELim.SurfWaterMass, 1.304141)
+    #
+    # assert np.isclose(output.log.final.LC17RRLim.EnvelopeMass, 0.000000)
+    # assert np.isclose(output.log.final.LC17RRLim.PlanetRadius, 1.063513)
+    # assert np.isclose(output.log.final.LC17RRLim.SurfWaterMass, 1.347668)
+    #
+    # assert np.isclose(output.log.final.LC17Auto.EnvelopeMass, 0.000000)
+    # assert np.isclose(output.log.final.LC17Auto.PlanetRadius, 1.063513)
+    # assert np.isclose(output.log.final.LC17Auto.SurfWaterMass, 1.347368)

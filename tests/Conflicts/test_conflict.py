21f8c799298d6be927e7d06a4354acd7b864b836
Rodrigo Luger <rodluger@gmail.com>
2021-06-02 10:49:17 -0400

    fix test conflict

--- tests/Conflicts/test_conflict.py
+++ tests/Conflicts/test_conflict.py
@@ -5,39 +5,50 @@ Raise error if merge conflicts are found.
 import pathlib
 import subprocess
 
-# Find all files containing this pattern
-pattern = "<<<<<<< HEAD"
 
-# Path to top level repo directory
-path = pathlib.Path(__file__).absolute().parents[2]
+def test_conflict():
+    # Find all files containing this pattern
+    pattern = "<<<<<<< HEAD"
 
-# Don't search for the pattern in this file or in .git
-this = pathlib.Path(__file__).name
-dotgit = path / ".git"
+    # Path to top level repo directory
+    path = pathlib.Path(__file__).absolute().parents[2]
 
-# Call grep
-process = subprocess.check_output(
-    [
-        "grep",
-        "-rnw",
-        "-e",
-        pattern,
-        f"--exclude={this}",
-        f"--exclude-dir={dotgit}",
-        path,
-    ]
-)
-results = process.decode().split("\n")
-results = [result for result in results if len(result)]
+    # Don't search for the pattern in this file or in .git
+    this = pathlib.Path(__file__).name
+    dotgit = path / ".git"
 
-# Raise AssertionError if pattern is found at least once
-if len(results) > 0:
-    for i in results:
-        info = i.split(" ")[0].rpartition(":")[0]
-        file = info.partition(":")[0]
-        line = info.partition(":")[-1]
-        print("File:", file)
-        print("Line Number:", line)
-    assert False
-else:
-    assert True
+    try:
+
+        # Call grep
+        process = subprocess.check_output(
+            [
+                "grep",
+                "-rnw",
+                "-e",
+                pattern,
+                f"--exclude={this}",
+                f"--exclude-dir={dotgit}",
+                path,
+            ]
+        )
+
+    except subprocess.CalledProcessError:
+
+        # Grep didn't find anything: this is good!
+        return
+
+    finally:
+
+        # Grep found something. Let's parse the output
+        results = process.decode().split("\n")
+        results = [result for result in results if len(result)]
+
+        # Raise AssertionError if pattern is found at least once
+        if len(results) > 0:
+            for i in results:
+                info = i.split(" ")[0].rpartition(":")[0]
+                file = info.partition(":")[0]
+                line = info.partition(":")[-1]
+                print("File:", file)
+                print("Line Number:", line)
+            assert False

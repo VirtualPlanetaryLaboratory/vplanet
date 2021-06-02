8bfad2dccbfae1407d54608b629d9a431ebeb684
Rodrigo Luger <rodluger@gmail.com>
2021-06-02 10:38:51 -0400

    fix conflict test

--- tests/Conflicts/test_conflict.py
+++ tests/Conflicts/test_conflict.py
@@ -1,20 +1,43 @@
-import os
+"""
+Raise error if merge conflicts are found.
+
+"""
+import pathlib
 import subprocess
 
+# Find all files containing this pattern
+pattern = "<<<<<<< HEAD"
 
-os.chdir("../../")
+# Path to top level repo directory
+path = pathlib.Path(__file__).absolute().parents[2]
 
-process = subprocess.check_output(['grep', '-rnw', '-e', "<<<<<<< HEAD"])
+# Don't search for the pattern in this file or in .git
+this = pathlib.Path(__file__).name
+dotgit = path / ".git"
 
-results = process.decode().split("\n")[0:-2]
+# Call grep
+process = subprocess.check_output(
+    [
+        "grep",
+        "-rnw",
+        "-e",
+        pattern,
+        f"--exclude={this}",
+        f"--exclude-dir={dotgit}",
+        path,
+    ]
+)
+results = process.decode().split("\n")
+results = [result for result in results if len(result)]
 
+# Raise AssertionError if pattern is found at least once
 if len(results) > 0:
     for i in results:
         info = i.split(" ")[0].rpartition(":")[0]
         file = info.partition(":")[0]
         line = info.partition(":")[-1]
         print("File:", file)
-        print("Line Number:",line)
+        print("Line Number:", line)
     assert False
 else:
     assert True

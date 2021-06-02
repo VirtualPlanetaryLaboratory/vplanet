d65c03cc94be25fe960347426214bde141b4cc4e
Rodrigo Luger <rodluger@gmail.com>
2021-06-02 11:57:59 -0400

    fix

--- tests/Conflicts/test_conflict.py
+++ tests/Conflicts/test_conflict.py
@@ -13,51 +13,35 @@ def test_conflict():
     # Path to top level repo directory
     path = pathlib.Path(__file__).absolute().parents[2]
 
-    # Exclude certain files / folders
-    exclude_files = [
-        pathlib.Path(__file__).name,  # this file
-        "*.pyc",  # temporary python files
-    ]
-    exclude_dirs = [
-        ".git",  # the .git metadata, which is massive
-        "__pycache__",  # temporary python files
-    ]
+    # Current git branch
+    branch = (
+        subprocess.check_output(["git", "branch", "--show-current"], cwd=path)
+        .decode()
+        .replace("\n", "")
+    )
 
-    # Format the exclusion rules
-    exclude_files = [
-        "--exclude={}".format(file.replace("*", "\*")) for file in exclude_files
-    ]
-    exclude_dirs = [
-        "--exclude={}".format(directory.replace("*", "\*"))
-        for directory in exclude_dirs
-    ]
-
-    try:
-
-        # Call grep
-        process = subprocess.check_output(
-            ["grep", "-rnw", "-e", pattern] + exclude_files + exclude_dirs + [path]
+    # Get all files tracked by git
+    files = (
+        subprocess.check_output(
+            ["git", "ls-tree", "-r", branch, "--name-only"], cwd=path
         )
+        .decode()
+        .split("\n")[:-1]
+    )
 
-    except subprocess.CalledProcessError:
+    # Search in all files
+    for file in files:
 
-        # Grep didn't find anything: this is good!
-        return
+        # Ignore this file!
+        if __file__ in str(path / file):
+            continue
 
-    # Grep found something. Let's parse the output
-    results = process.decode().split("\n")
-    results = [result for result in results if len(result)]
-
-    # Raise AssertionError if pattern is found at least once
-    if len(results) > 0:
-
-        # DEBUG
-        print(results)
-
-        for i in results:
-            info = i.split(" ")[0].rpartition(":")[0]
-            file = info.partition(":")[0]
-            line = info.partition(":")[-1]
-            print("File:", file)
-            print("Line Number:", line)
-        assert False
+        try:
+            output = subprocess.check_output(
+                ["grep", "-nw", "-e", pattern, path / file]
+            ).decode()
+        except subprocess.CalledProcessError:
+            # Error means grep didn't find anything
+            pass
+        else:
+            raise ValueError(f"Merge conflict in {file}, line {output}")

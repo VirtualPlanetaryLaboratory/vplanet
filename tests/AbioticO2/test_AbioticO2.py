a595f983ec40d9b965d89b8224143643a519690e
Rory Barnes <rory@proxima.astro.washington.edu>
2021-12-21 14:23:45 -0800

    Updated values in tests/AbioticO2.

--- tests/AbioticO2/test_AbioticO2.py
+++ tests/AbioticO2/test_AbioticO2.py
@@ -4,8 +4,8 @@ import astropy.units as u
 
 @benchmark(
     {
-        "log.final.star.Luminosity": {"value": 7.362835e23, "unit": u.W},
-        "log.final.star.LXUVStellar": {"value": 7.362835e20, "unit": u.W},
+        "log.final.star.Luminosity": {"value": 7.362896e+23, "unit": u.W},
+        "log.final.star.LXUVStellar": {"value": 7.362896e+20, "unit": u.W},
         "log.final.star.Radius": {"value": 1.186502e08, "unit": u.m},
         "log.final.star.Temperature": {"value": 2926.556751, "unit": u.Kelvin},
         "log.final.star.RadGyra": {"value": 0.466090},
@@ -13,9 +13,9 @@ import astropy.units as u
         "log.final.b.OxygenMass": {"value": 209.805999, "unit": u.bar, "rtol": 1e-4},
         "log.final.e.SurfWaterMass": {"value": 7.403340, "unit": u.TO},
         "log.final.e.OxygenMass": {"value": 418.194489, "unit": u.bar},
-        "log.final.e.FXUV": {"value": 3.053257, "unit": u.W / u.m ** 2},
+        "log.final.e.FXUV": {"value": 3.053282, "unit": u.W / u.m ** 2},
         "log.final.e.AtmXAbsEffH2O": {"value": 0.051776},
-        "log.final.e.Instellation": {"value": 3053.257033, "unit": u.kg / u.sec ** 3},
+        "log.final.e.Instellation": {"value": 3053.282097, "unit": u.kg / u.sec ** 3},
     }
 )
 class TestAbioticO2(Benchmark):

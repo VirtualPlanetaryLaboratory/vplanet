455bd73b4ad19925e7e2a042a8742748575b4b18
caitlyn-wilhelm <cwilhelm@uw.edu>
2022-01-13 12:33:35 -0800

    valgrind test and yml file

--- tests/valgrind.py
+++ tests/valgrind.py
@@ -3,14 +3,20 @@ import numpy as np
 import os
 import sys
 
-subdir = [f.name for f in os.scandir('.') if f.is_dir()]
+subdir = sorted([f.name for f in os.scandir('.') if f.is_dir()])
 
 # First make debug
-sys.stdout.write('Making VPLanet...')
+print('Making VPLanet...')
 sys.stdout.flush()
-os.chdir('..')
-subprocess.run(['make debug >& /dev/null'], shell=True)
-os.chdir('tests')
+os.chdir('../')
+#print(os.getcwd())
+#subprocess.run(['make opt >& /dev/null'], shell=True)
+with open('make_log','w') as m:
+    
+    subprocess.run('make opt',shell=True, stderr= m,stdout= m)
+    subprocess.run(['rm','make_log'])
+
+os.chdir('tests/')
 
 print('done.')
 
@@ -19,24 +25,35 @@ tot_test = 0
 for sub in subdir:
     tot_test += 1
     sys.stdout.write(sub)
+    #sys.stdout.write("")
     sys.stdout.flush()
     os.chdir(sub)
-    fout = sub+'.valgrind'
-    cmd='valgrind --track-origins=yes ../../vplanet vpl.in >& '+fout
-    subprocess.run(cmd, shell=True)
+    fout = sub +'.valgrind'
+    #cmd='/usr/bin/valgrind --track-origins=yes ../../bin/vplanet vpl.in &> '+ fout
+    cmd='/usr/bin/valgrind --track-origins=yes ../../bin/vplanet vpl.in '
+
+    #print(cmd)
+    #cmd = cmd.split()
+    
+    with open(fout,"w+") as f:
+        subprocess.run(cmd, shell=True,stdout=f,stderr=f)
     f = open(fout, "r")
     last_line = f.readlines()[-1]
+    #print(last_line)
     f.close()
     words = last_line.split()
     n_errors = int(words[3])
     if n_errors > 0:
         tot_fail += 1
     print(': '+repr(n_errors)+' error(s)')
-    os.chdir('..')
+    os.chdir('../')
+    
 
-sys.stdout.write('Done! ')
+print('Done! ')
 
 if (tot_fail == 0):
     print('VPLanet is mem-check-clean!')
+    assert(True)
 else:
     print(repr(tot_fail)+'/'+repr(tot_test)+' test(s) failed.')
+    assert(False)

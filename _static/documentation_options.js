2443fbfdc64b82d84a94c4addba107e35340f1a9
RoryBarnes <rory@astro.washington.edu>
2023-03-14 17:29:05 +0000

    Deploying to gh-pages from @ VirtualPlanetaryLaboratory/vplanet@269eaa2e5efe2d49c385b5dc4d388fa070aeda35 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3.37',
+    VERSION: '2.3.38',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

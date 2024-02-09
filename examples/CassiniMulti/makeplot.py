530a05017c2f6f8e855e53e9e55fe94edba2750a
Rory Barnes <rory@proxima.astro.washington.edu>
2024-02-09 13:49:12 -0800

    Finished added the 2nd order secular model to CassiniMulti.

--- examples/CassiniMulti/makeplot.py
+++ examples/CassiniMulti/makeplot.py
@@ -26,18 +26,21 @@ plt.plot(time, rd4.TGb.Obliquity, color=vplot.colors.dark_blue)
 plt.plot(time, rd4.TGc.Obliquity, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel(r"Obliquity ($^\circ$)")
+plt.legend('', frameon=False) # Why do we need these!??
 
 plt.subplot(3, 2, 2)
 plt.plot(time, rd4.TGb.Eccentricity, color=vplot.colors.dark_blue)
 plt.plot(time, rd4.TGc.Eccentricity, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("Eccentricity")
+plt.legend('', frameon=False)
 
 plt.subplot(3, 2, 3)
 plt.plot(time, rd4.TGb.RotPer, color=vplot.colors.dark_blue)
 plt.plot(time, rd4.TGc.RotPer, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("Rotation Period (days)")
+plt.legend('', frameon=False)
 
 plt.subplot(3, 2, 4)
 plt.plot(time, rd4.TGb.DynEllip, color=vplot.colors.dark_blue, label="b")
@@ -51,12 +54,14 @@ plt.plot(time, rd4.TGb.CassiniOne, color=vplot.colors.dark_blue)
 plt.plot(time, rd4.TGc.CassiniOne, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("$\sin{\Psi}$")
+plt.legend('', frameon=False)
 
 plt.subplot(3, 2, 6)
 plt.plot(time, rd4.TGb.CassiniTwo, color=vplot.colors.dark_blue)
 plt.plot(time, rd4.TGc.CassiniTwo, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("$\cos{\Psi}$")
+plt.legend('', frameon=False)
 
 # Save the figure
 ext = get_args().ext
@@ -71,18 +76,21 @@ plt.plot(time, ll2.TGb.Obliquity, color=vplot.colors.dark_blue)
 plt.plot(time, ll2.TGc.Obliquity, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel(r"Obliquity ($^\circ$)")
+plt.legend('', frameon=False)
 
 plt.subplot(3, 2, 2)
 plt.plot(time, ll2.TGb.Eccentricity, color=vplot.colors.dark_blue)
 plt.plot(time, ll2.TGc.Eccentricity, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("Eccentricity")
+plt.legend('', frameon=False)
 
 plt.subplot(3, 2, 3)
 plt.plot(time, ll2.TGb.RotPer, color=vplot.colors.dark_blue)
 plt.plot(time, ll2.TGc.RotPer, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("Rotation Period (days)")
+plt.legend('', frameon=False)
 
 plt.subplot(3, 2, 4)
 plt.plot(time, ll2.TGb.DynEllip, color=vplot.colors.dark_blue, label="b")
@@ -96,12 +104,14 @@ plt.plot(time, ll2.TGb.CassiniOne, color=vplot.colors.dark_blue)
 plt.plot(time, ll2.TGc.CassiniOne, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("$\sin{\Psi}$")
+plt.legend('', frameon=False)
 
 plt.subplot(3, 2, 6)
 plt.plot(time, ll2.TGb.CassiniTwo, color=vplot.colors.dark_blue)
 plt.plot(time, ll2.TGc.CassiniTwo, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("$\cos{\Psi}$")
+plt.legend('', frameon=False)
 
 # Save the figure
 ext = get_args().ext

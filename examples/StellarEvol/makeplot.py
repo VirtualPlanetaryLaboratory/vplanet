03ea6a9ad3a29c48830f1e706b4e2e515fe64fa6
Rory Barnes <rory@proxima.astro.washington.edu>
2025-05-12 17:51:32 -0700

    Changed x-axis in examples/StellarEvol to be Age.

--- examples/StellarEvol/makeplot.py
+++ examples/StellarEvol/makeplot.py
@@ -27,7 +27,7 @@ dAge                      5e6
 sStellarModel             baraffe
 dSatXUVFrac               1.e-3
 dSatXUVTime               -0.1
-saOutputOrder Time -Luminosity -Radius Temperature -RotPer -LXUVTot RadGyra
+saOutputOrder Time Age -Luminosity -Radius Temperature -RotPer -LXUVTot RadGyra
 """
 
 # System input file template
@@ -70,37 +70,37 @@ def run(masses):
     """Run vplanet and collect the output."""
     write_in(masses)
     output = vplanet.run(path / "vpl.in", units=False)
-    time = output.bodies[0].Time
+    age = output.bodies[0].Age
     radius = [output.bodies[n].Radius for n in range(len(masses))]
     temp = [output.bodies[n].Temperature for n in range(len(masses))]
     lum = [output.bodies[n].Luminosity for n in range(len(masses))]
     lxuv = [output.bodies[n].LXUVTot for n in range(len(masses))]
     prot = [output.bodies[n].RotPer for n in range(len(masses))]
     rg = [output.bodies[n].RadGyra for n in range(len(masses))]
-    return time, radius, lum, lxuv, temp, prot, rg
+    return age, radius, lum, lxuv, temp, prot, rg
 
 
 # Create the figure
-fig, ax = plt.subplots(nrows=3, ncols=2, figsize=(10, 6))
+fig, ax = plt.subplots(nrows=3, ncols=2, figsize=(6.5, 9))
 fig.subplots_adjust(right=0.825, wspace=0.30)
 
 masses = np.array([0.08, 0.25, 0.5, 0.75, 1.0])
-time, radius, lum, lxuv, temp, prot, rg = run(masses)
+age, radius, lum, lxuv, temp, prot, rg = run(masses)
 
 # Plot stars that survived
 for n, m in enumerate(masses):
     # Top row: radius, legend
-    ax[0, 0].plot(time, radius[n], label="%.2f" % m, color=cmap(0.7 * m))
+    ax[0, 0].plot(age, radius[n], label="%.2f" % m, color=cmap(0.7 * m))
     # Dummy data for legend
     ax[0, 1].plot([101], [100], label="%.2f" % m, color=cmap(0.7 * m))
 
     # Middle row: rg, Teff
-    ax[1, 0].plot(time, rg[n], label="%.2f" % m, color=cmap(0.7 * m))
-    ax[1, 1].plot(time, temp[n], label="%.2f" % m, color=cmap(0.7 * m))
+    ax[1, 0].plot(age, rg[n], label="%.2f" % m, color=cmap(0.7 * m))
+    ax[1, 1].plot(age, temp[n], label="%.2f" % m, color=cmap(0.7 * m))
 
     # Bottom row: L, Lxuv
-    ax[2, 0].plot(time, lum[n], label="%.2f" % m, color=cmap(0.7 * m))
-    ax[2, 1].plot(time, lxuv[n], label="%.2f" % m, color=cmap(0.7 * m))
+    ax[2, 0].plot(age, lum[n], label="%.2f" % m, color=cmap(0.7 * m))
+    ax[2, 1].plot(age, lxuv[n], label="%.2f" % m, color=cmap(0.7 * m))
 
 # Add in dead star
 vplanet.run(path / "M1.3" / "vpl.in", units=False)
@@ -108,19 +108,19 @@ dead = np.genfromtxt(path / "M1.3" / "system.a.forward", delimiter=" ")
 
 # Top row: radius, legend
 m = 1.3
-time = dead[:, 0]
-ax[0, 0].plot(time, dead[:, 4], label="%.2f" % m, color=cmap(0.7 * m))
+age = dead[:, 1]
+ax[0, 0].plot(age, dead[:, 5], label="%.2f" % m, color=cmap(0.7 * m))
 
 # Dummy data for legend
 ax[0, 1].plot([101], [100], label="%.2f" % m, color=cmap(0.7 * m))
 
 # Middle row: rg, Teff
-ax[1, 0].plot(time, dead[:, 8], label="%.2f" % m, color=cmap(0.7 * m))
-ax[1, 1].plot(time, dead[:, 5], label="%.2f" % m, color=cmap(0.7 * m))
+ax[1, 0].plot(age, dead[:, 9], label="%.2f" % m, color=cmap(0.7 * m))
+ax[1, 1].plot(age, dead[:, 6], label="%.2f" % m, color=cmap(0.7 * m))
 
 # Bottom row: L, Lxuv
-ax[2, 0].plot(time, dead[:, 3], label="%.2f" % m, color=cmap(0.7 * m))
-ax[2, 1].plot(time, dead[:, 7], label="%.2f" % m, color=cmap(0.7 * m))
+ax[2, 0].plot(age, dead[:, 4], label="%.2f" % m, color=cmap(0.7 * m))
+ax[2, 1].plot(age, dead[:, 8], label="%.2f" % m, color=cmap(0.7 * m))
 
 for axis in ax.flatten():
     axis.set_xscale("log")
@@ -129,16 +129,17 @@ ax[0, 1].set_xlim(0, 1)
 ax[0, 1].set_axis_off()
 ax[2, 0].set_yscale("log")
 ax[2, 1].set_yscale("log")
-ax[2, 0].set_xlabel("Time (yr)", fontsize=14)
-ax[2, 1].set_xlabel("Time (yr)", fontsize=14)
+ax[2, 0].set_xlabel("Age (yr)", fontsize=14)
+ax[2, 1].set_xlabel("Age (yr)", fontsize=14)
 ax[0, 0].set_ylabel(r"Radius ($\mathrm{R}_\odot$)", fontsize=14)
 ax[1, 0].set_ylabel(r"Radius of Gyration", fontsize=14)
 ax[2, 0].set_ylabel(r"Luminosity ($\mathrm{L}_\odot$)", fontsize=14)
 ax[1, 1].set_ylabel(r"Temperature ($\mathrm{K}$)", fontsize=14)
 ax[2, 1].set_ylabel(r"L$_{\mathrm{XUV}}$ ($\mathrm{L}_\odot$)", fontsize=14)
-leg = ax[0, 1].legend(loc=(-0.2, 0.15), title=r"Mass ($\mathrm{M}_\odot$)", ncol=3)
+#leg = ax[0, 1].legend(loc=(-0.2, 0.15), title=r"Mass ($\mathrm{M}_\odot$)", ncol=3)
+leg = ax[0, 1].legend(loc=(-0.15, 0.35), title=r"Mass ($\mathrm{M}_\odot$)", ncol=3)
 leg.get_title().set_fontweight("bold")
 
 # Save
 ext = get_args().ext
-fig.savefig(path / f"StellarEvol.{ext}", bbox_inches="tight", dpi=600)
+fig.savefig(path / f"StellarEvol.{ext}", dpi=300)
